e7d4e8ecec420294981167a05ca79b86d88f05e6
Denys Vitali <denys@denv.it>
2018-11-15 15:48:45 +0100

    Initial commit

---
+++ include/library.h
@@ -0,0 +1,6 @@
+#ifndef TUNA_GE_LIBRARY_H
+#define TUNA_GE_LIBRARY_H
+
+void hello();
+
+#endif
---
+++ library.cpp
@@ -0,0 +1,7 @@
+#include "include/library.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "I'm the library." << std::endl;
+}
