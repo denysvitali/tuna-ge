e4bdaf1416d75dd60dde8d2ee5a86fe5b66a438d
Denys Vitali <denys@denv.it>
2018-11-19 13:44:10 +0100

    Edit gitignore, fix camera coordinates comment

--- tuna-ge.cpp
+++ tuna-ge.cpp
@@ -22,7 +22,7 @@ bool TunaGE::debug = true;
 RGBColor TunaGE::color = RGBColor(0,0,0);
 int TunaGE::windowId = -1;
 glm::mat4 TunaGE::camera = glm::lookAt(
-        glm::vec3(0,3,3), // Camera is at (4,3,3), in World Space
+        glm::vec3(0,3,3), // Camera is at (0,3,3), in World Space
         glm::vec3(0,0,0), // and looks at the origin
         glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
 );
