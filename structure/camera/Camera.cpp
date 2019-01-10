5d97a640267791fae8992324887ff5297c7b8d0e
Denys Vitali <denys@denv.it>
2019-01-10 12:58:04 +0100

    Camera: Reformat

--- structure/camera/Camera.cpp
+++ structure/camera/Camera.cpp
@@ -1,106 +1,88 @@
 #include "Camera.h"
 #include <GL/freeglut.h>
 
-glm::mat4 tunage::Camera::getInverseMatrix() const
-{
+glm::mat4 tunage::Camera::getInverseMatrix() const {
 	return glm::inverse(getMatrix());
 }
 
-glm::mat4 tunage::Camera::getProjectionMatrix() const
-{
-	return glm::perspective(glm::radians(FOVangle), (float)(screen_w) / (float)(screen_h), nearPlane, farPlane);
+glm::mat4 tunage::Camera::getProjectionMatrix() const {
+	return glm::perspective(glm::radians(FOVangle), (float) (screen_w) / (float) (screen_h), nearPlane, farPlane);
 }
 
-void tunage::Camera::loadProjectionMatrix()
-{
+void tunage::Camera::loadProjectionMatrix() {
 	glMatrixMode(GL_PROJECTION);
-	glm::mat4 Projection = glm::perspective(glm::radians(FOVangle), (float)(screen_w) / (float)(screen_h), nearPlane, farPlane);
+	glm::mat4 Projection = glm::perspective(glm::radians(FOVangle), (float) (screen_w) / (float) (screen_h), nearPlane,
+											farPlane);
 	glLoadMatrixf(glm::value_ptr(Projection));
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void tunage::Camera::setFOV(float FOVangle)
-{
+void tunage::Camera::setFOV(float FOVangle) {
 	this->FOVangle = FOVangle;
 }
 
-float tunage::Camera::getFOV() const
-{
+float tunage::Camera::getFOV() const {
 	return FOVangle;
 }
 
-void tunage::Camera::setNearPlane(float nearPlane)
-{
+void tunage::Camera::setNearPlane(float nearPlane) {
 	this->nearPlane = nearPlane;
 }
 
-float tunage::Camera::getNearPlane() const
-{
+float tunage::Camera::getNearPlane() const {
 	return nearPlane;
 }
 
-void tunage::Camera::setFarPlane(float farPlane)
-{
+void tunage::Camera::setFarPlane(float farPlane) {
 	this->farPlane = farPlane;
 }
 
-float tunage::Camera::getFarPlane() const
-{
+float tunage::Camera::getFarPlane() const {
 	return farPlane;
 }
 
-void tunage::Camera::setScreenSize(int screen_w, int screen_h)
-{
+void tunage::Camera::setScreenSize(int screen_w, int screen_h) {
 	this->screen_w = screen_w;
 	this->screen_h = screen_h;
 }
 
-void tunage::Camera::setProjMode(bool projMode)
-{
+void tunage::Camera::setProjMode(bool projMode) {
 	this->projMode = projMode;
 }
 
-void tunage::Camera::setCameraPos(glm::vec3 cameraPos)
-{
+void tunage::Camera::setCameraPos(glm::vec3 cameraPos) {
 	this->cameraPos = cameraPos;
 }
 
-glm::vec3 tunage::Camera::getCameraPos() const
-{
+glm::vec3 tunage::Camera::getCameraPos() const {
 	return cameraPos;
 }
 
-void tunage::Camera::setCameraFront(glm::vec3 cameraFront)
-{
+void tunage::Camera::setCameraFront(glm::vec3 cameraFront) {
 	this->cameraFront = cameraFront;
 }
 
-glm::vec3 tunage::Camera::getCameraFront() const
-{
+glm::vec3 tunage::Camera::getCameraFront() const {
 	return cameraFront;
 }
 
-void tunage::Camera::setCameraUp(glm::vec3 cameraUp)
-{
+void tunage::Camera::setCameraUp(glm::vec3 cameraUp) {
 	this->cameraUp = cameraUp;
 }
 
-glm::vec3 tunage::Camera::getCameraUp() const
-{
+glm::vec3 tunage::Camera::getCameraUp() const {
 	return cameraUp;
 }
 
-void tunage::Camera::setCameraSpeed(float cameraSpeed)
-{
+void tunage::Camera::setCameraSpeed(float cameraSpeed) {
 	this->cameraSpeed = cameraSpeed;
 }
 
-float tunage::Camera::getCameraSpeed() const
-{
+float tunage::Camera::getCameraSpeed() const {
 	return cameraSpeed;
 }
-void tunage::Camera::updateCamera()
-{
+
+void tunage::Camera::updateCamera() {
 	this->setMatrix(glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp));
 }
 
