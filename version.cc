54300257d95c89cce8484975b0475773aabb2d9b
Raenilol <raenilol@gmail.com>
2019-03-15 17:07:50 +0100

    Remove VAO class

--- version.cc
+++ version.cc
@@ -5,6 +5,6 @@
 #endif
 
 using namespace tunage;
-const std::string& Version::GIT_SHA1 = "3db68b1-dirty";
-const std::string& Version::GIT_DATE = "Fri Mar 15 13:46:10 2019";
-const std::string& Version::GIT_COMMIT_SUBJECT = "shaders WIP";
+const std::string& Version::GIT_SHA1 = "783b0c8";
+const std::string& Version::GIT_DATE = "Fri Mar 15 15:57:36 2019";
+const std::string& Version::GIT_COMMIT_SUBJECT = "Ping Phong";
